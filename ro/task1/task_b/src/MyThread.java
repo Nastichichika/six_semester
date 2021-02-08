9fa10311f4a77c1154c37d44bfd3fb74203302a6
Anastasiia <0956963312n@gmail.com>
2021-02-08 20:35:24 +0200

    finished task1_b ro

--- ro/task1/task_b/src/MyThread.java
+++ ro/task1/task_b/src/MyThread.java
@@ -1,12 +1,12 @@
 import javax.swing.*;
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
 
 class MyThread implements Runnable {
     final JSlider slider1;
     boolean operation;
-    Semaphore semaphore;
+    AtomicInteger semaphore;
 
-    public MyThread(JSlider slider1, boolean operation, Semaphore semaphore) {
+    public MyThread(JSlider slider1, boolean operation, AtomicInteger semaphore) {
         this.slider1 = slider1;
         this.operation = operation;
         this.semaphore = semaphore;
@@ -18,16 +18,11 @@ class MyThread implements Runnable {
             new_value = -1;
         else
             new_value = 1;
-
-        try {
-            semaphore.acquire();
-            while(semaphore.availablePermits() == 0) {
-                synchronized (slider1) {
-                    slider1.setValue(new_value + slider1.getValue());
-                }
+        System.out.println(semaphore.get());
+        while(semaphore.intValue() != 0) {
+            synchronized (slider1) {
+                slider1.setValue(new_value + slider1.getValue());
             }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
     }
 } 
--- ro/task1/task_b/src/Window.java
+++ ro/task1/task_b/src/Window.java
@@ -10,8 +10,9 @@ public class Window extends JFrame
     {
         super("Task1_b");
 
-        Semaphore semaphore = new Semaphore(1);
-       
+        //Semaphore semaphore = new Semaphore(1);
+        AtomicInteger semaphore = new AtomicInteger(0);
+
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         MySlider slider1 = new MySlider(10, 90, 50);
@@ -24,7 +25,6 @@ public class Window extends JFrame
 
         JButton start1 = new JButton("Пуск1");
         contents.add(start1, BorderLayout.SOUTH);
-        AtomicInteger t_num = new AtomicInteger();
 
         JButton start2 = new JButton("Пуск2");
         contents.add(start2);
@@ -37,52 +37,39 @@ public class Window extends JFrame
 
         getContentPane().add(contents);
 
-
         start1.addActionListener(
                 e ->{
-                    int check = semaphore.availablePermits();
-                    if(check == 0) {
-                        JOptionPane.showMessageDialog(contents, "Занято потоком");
-                        return;
+                    if(semaphore.compareAndSet(0,1)) {
+                        Thread thread1 = new Thread(new MyThread(slider1, true, semaphore),"MyThread");
+                        thread1.start();
+                        thread1.setPriority(1);
                     }
-
-                    Thread thread1 = new Thread(new MyThread(slider1, true, semaphore),"MyThread");
-                    thread1.start();
-                    thread1.setPriority(1);
-                    t_num.set(1);
-        });
+                    else {
+                        JOptionPane.showMessageDialog(contents, "Занято потоком");
+                    }
+                });
 
         start2.addActionListener(
                 e -> {
-                    int check = semaphore.availablePermits();
-                    if(check == 0) {
-                        JOptionPane.showMessageDialog(contents, "Занято потоком");
-                        return;
+                    if(semaphore.compareAndSet(0,2)) {
+                        Thread thread2 = new Thread(new MyThread(slider1, false, semaphore), "MyThread");
+                        thread2.start();
+                        thread2.setPriority(1);
                     }
-
-                    Thread thread2 = new Thread(new MyThread(slider1, false, semaphore),"MyThread");
-                    thread2.start();
-                    thread2.setPriority(1);
-
-                    t_num.set(2);
+                    else {
+                        JOptionPane.showMessageDialog(contents, "Занято потоком");
+                    }
         });
 
         stop1.addActionListener(
                 e -> {
-                    if(t_num.get() != 1){
-                        return;
-                    }
-                    semaphore.release();
-                    System.out.println(semaphore.availablePermits());
+                    semaphore.compareAndSet(1,0);
         });
 
         stop2.addActionListener(
                 e -> {
-                    if(t_num.get() != 2){
-                        return;
-                    }
-                    semaphore.release();
-                    System.out.println(semaphore.availablePermits());
+
+                    semaphore.compareAndSet(2,0);
         });
         setSize(330, 170);
         setVisible(true);
